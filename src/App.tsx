a36b26eebfa4d18fe18805b4edc34e540eb42860
Hayeon-Jung <jhy000714@gmail.com>
2025-07-24 18:01:39 +0900

    사용하지 않는 변수 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Calendar from "./layout/Calendar";
+// import Calendar from "./layout/Calendar";
 import Test from "./layout/Test";
 
 function App() {
