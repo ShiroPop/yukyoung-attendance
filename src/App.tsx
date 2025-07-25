b122329e4fd5a14a39249d4bbf554e75256fcaf4
Hayeon-Jung <jhy000714@gmail.com>
2025-07-25 16:29:16 +0900

    클래스 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Calendar from "./layout/Calendar";
-// import Calendar from "./layout/Calendar";
+
+import Classes from "./layout/Classes";
 import Test from "./layout/Test";
 
 function App() {
   return (
     <div className="App">
       <Calendar />
+      <Classes />
+
       <Test />
     </div>
   );
