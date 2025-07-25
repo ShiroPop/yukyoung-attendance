dd2848d3229479e6472d1540b2d99f2ef8b13cd5
Hayeon-Jung <jhy000714@gmail.com>
2025-07-25 16:28:41 +0900

    문자 레이아웃 조절

--- src/layout/Calendar.tsx
+++ src/layout/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { fetchCollection } from "../utils/firestore";
 import styled from "styled-components";
 
@@ -9,13 +9,25 @@ interface date {
 
 const CalendarTable = styled.table`
   width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
   border-collapse: collapse;
   table-layout: fixed;
 `;
 
+const ThPadding = styled.th`
+  padding: 4px;
+`;
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  min-width: 36px;
+`;
+
 const TablePadding = styled.td`
   text-align: center;
-  padding: 8px;
+  padding: 4px;
 `;
 
 const DateInput = styled.input.attrs({ type: "radio" })`
@@ -56,22 +68,28 @@ const Cell = styled.div<{ isCurrent: boolean }>`
   font-size: 18px;
   font-weight: 400;
   line-height: 120%;
-  cursor: default;
-  width: 44px;
-  height: 44px;
+  width: 36px;
+  height: 36px;
   border-radius: 99px;
+  cursor: pointer;
 `;
 
-const Input = styled.div``;
+//
+// firebase db 조인해서 가져오기
+// 클래스 바 구현 후, join한 date 값을 가지고 cell css 수정하기
+// 최종적으로 나온 cell 디자인을 보고 table min width 주기 (원이 겹치지 않는 사이즈)
+// 데이터 캐싱 관련해서 react query 고민해보기
+// 달력함수 useMemo 고려해보기
+//
 
 const Calendar = () => {
   const [year, setYear] = useState(new Date().getFullYear());
   const [month, setMonth] = useState(new Date().getMonth());
-  const [dateArray, setDateArray] = useState<Array<Array<date>>>([]);
+  // const [dateArray, setDateArray] = useState<Array<Array<date>>>([]);
 
   const weekDays = ["일", "월", "화", "수", "목", "금", "토"];
 
-  useEffect(() => {
+  const dateArray = useMemo(() => {
     const firstDay = new Date(year, month, 1).getDay();
     const daysInCurrentMonth = new Date(year, month + 1, 0).getDate();
     const daysInPreviousMonth = new Date(year, month, 0).getDate();
@@ -87,8 +105,8 @@ const Calendar = () => {
     }));
 
     const allDates = [...prevMonthDates, ...currentMonthDates];
-
     const nextDaysCount = (7 - (allDates.length % 7)) % 7;
+
     const nextMonthDates = Array.from({ length: nextDaysCount }, (_, i) => ({
       day: i + 1,
       currentMonth: false,
@@ -101,8 +119,8 @@ const Calendar = () => {
       weeks.push(fullDates.slice(i, i + 7));
     }
 
-    setDateArray(weeks);
-  }, [month, year]);
+    return weeks;
+  }, [year, month]);
 
   const handleMonth = (next: boolean) => {
     if (next) {
@@ -131,8 +149,10 @@ const Calendar = () => {
       <CalendarTable>
         <thead>
           <tr>
-            {weekDays.map((ele) => (
-              <th>{ele}</th>
+            {weekDays.map((ele, idx) => (
+              <ThPadding key={idx}>
+                <Div>{ele}</Div>
+              </ThPadding>
             ))}
           </tr>
         </thead>
