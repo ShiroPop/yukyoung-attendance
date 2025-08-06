9fcc14e78587bfb79fd0dda0a89e055b0a6bf0ef
Hayeon-Jung <jhy000714@gmail.com>
2025-08-06 11:23:02 +0900

    로그인 css

--- src/layout/Login.tsx
+++ src/layout/Login.tsx
@@ -12,24 +12,75 @@ const Container = styled.div`
   align-items: center;
 `;
 
-const Input = styled.input``;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
-const Button = styled.button``;
+const Input = styled.input`
+  height: 38px;
+  width: 224px;
+  border: 1px solid #4caf50;
+  border-radius: 10px;
+  outline: none;
+  padding: 0 8px;
+
+  &:focus {
+    border: 1px solid #4caf50;
+    outline: 1px solid #4caf50;
+  }
+  &::placeholder {
+    color: #76c078;
+  }
+`;
+
+const ErrorMessage = styled.span<{ $visibility: boolean }>`
+  position: relative;
+  min-height: 24px;
+  top: 2px;
+  font-size: 13px;
+  font-weight: 600;
+  color: #ff5959;
+  visibility: ${({ $visibility }) => ($visibility ? "visible" : "hidden")};
+`;
+
+const Button = styled.button`
+  height: 44px;
+  width: 240px;
+  margin-bottom: 20px;
+  border: none;
+  border-radius: 99px;
+  background-color: #76c078;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.5s, color 0.2s;
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      background-color: #76c0784d;
+    }
+  }
+`;
 
 const Login = () => {
   const { loginId } = useAuth();
   const { loginError } = useUserStore();
   const [user, setUser] = useState("");
 
-  const handleLogin = async () => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     await loginId(user);
   };
   return (
     <>
       <Container>
-        <Input type="text" placeholder="아이디를 입력해주세요." onChange={(e) => setUser(e.target.value)} />
-        <div>{loginError}</div>
-        <Button onClick={handleLogin}>로그인</Button>
+        <Form onSubmit={handleLogin}>
+          <Input type="text" placeholder="아이디를 입력해주세요." onChange={(e) => setUser(e.target.value)} />
+          <ErrorMessage $visibility={!!loginError}>{loginError || " "}</ErrorMessage>
+          <Button type="submit">로그인</Button>
+        </Form>
       </Container>
     </>
   );
