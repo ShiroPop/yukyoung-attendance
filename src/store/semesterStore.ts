f88d852215f305f9686684f653d75ddf24b5b08b
Hayeon-Jung <jhy000714@gmail.com>
2025-08-06 13:58:58 +0900

    학기 변경

--- src/store/semesterStore.ts
+++ src/store/semesterStore.ts
@@ -6,6 +6,6 @@ interface SemesterStore {
 }
 
 export const useSemesterStore = create<SemesterStore>((set) => ({
-  semester: "2025-03-03",
+  semester: "2025-08-25",
   setSemester: (semester) => set({ semester }),
 }));
