12c8d9d9c5a6ec7bd4a4d00d0afe5bc6b8772946
Hayeon-Jung <jhy000714@gmail.com>
2025-08-07 17:09:43 +0900

    반별 학생을 불러오는 함수 추가

---
+++ src/api/useAssignedClassesStudent.ts
@@ -0,0 +1,56 @@
+import { useQueries } from "@tanstack/react-query";
+import { fetchCollection } from "../utils/fetchCollection";
+import { useClassesQuery } from "./useQuery";
+import { useSemesterStore } from "../store/semesterStore";
+
+interface NormalizedStudent {
+  id: string;
+  name: string;
+  classId: string;
+  monday: number;
+  tuesday: number;
+  wednesday: number;
+  thursday: number;
+  friday: number;
+}
+
+const normalizeStudent = (stu: any, classId: string) => ({
+  id: stu.id,
+  name: stu.name,
+  classId: stu.class || classId,
+});
+
+export const useAssignedClassesStudents = () => {
+  const { semester } = useSemesterStore();
+  const { data: assignedClasses } = useClassesQuery();
+
+  const queries = useQueries({
+    queries: (assignedClasses ?? []).map((cls) => ({
+      queryKey: ["students", semester, cls.id],
+      queryFn: async () => {
+        const students = await fetchCollection(["semester", semester, "class", cls.id, "student"]);
+        return students.map((stu) => normalizeStudent(stu, cls.id));
+      },
+      enabled: !!semester,
+      staleTime: 1000 * 60 * 5,
+    })),
+  });
+
+  const isLoading = queries.some((q) => q.isLoading);
+  const isError = queries.some((q) => q.isError);
+
+  // 반별로 구분된 형태로 리턴
+  const studentsByClass = (assignedClasses ?? []).reduce((acc, cls, idx) => {
+    acc[cls.id] = queries[idx]?.data ?? [];
+    return acc;
+  }, {} as Record<string, NormalizedStudent[]>);
+
+  // 전체 학생 리스트
+  // const allStudents = Object.values(studentsByClass).flat();
+
+  return {
+    isLoading,
+    isError,
+    studentsByClass,
+  };
+};
