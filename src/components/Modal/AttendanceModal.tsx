1d32f91dc6b04a2139e227684c11d5891fa77aa2
Hayeon-Jung <jhy000714@gmail.com>
2025-08-20 17:31:59 +0900

    useEffect 의존성 배열 추가

--- src/components/Modal/AttendanceModal.tsx
+++ src/components/Modal/AttendanceModal.tsx
@@ -73,8 +73,10 @@ const AttendanceModal = () => {
       }));
 
       setAttendances(result);
+
+      console.log("sia");
     }
-  }, [mergedByClass, attendanceRecords, classId, selectedDate, semester]);
+  }, [mergedByClass, attendanceRecords, classMember, classId, selectedDate, semester]);
 
   return (
     <ModalWrap $isModal={isModal} onClick={closeModal}>
